3e91d27424a301e0847692bc4c4b34302612b661
bmoran17 <brendamoran1999@gmail.com>
2023-04-18 23:08:32 -0700

    cont. stories component & started posts component

--- src/components/page/posts/Posts.jsx
+++ src/components/page/posts/Posts.jsx
@@ -2,8 +2,65 @@ import React from "react";
 import "./posts.css";
 
 const Posts = () => {
+  const postsInfo = [
+    {
+      "name":"cat01",
+      "likes": 45,
+      "caption": "cat01",
+      "imagePosted": "./post's-profiles/pexels-peng-louis-1643457.jpg",
+      "comments": [],
+      "timePosted": "24m"
+    },
+    {
+      "name":"cat02",
+      "likes": 164,
+      "caption": "cat02",
+      "imagePosted": "./post's-profiles/pexels-tranmautritam-2194261.jpg",
+      "comments": [],
+      "timePosted": "1h"
+    },
+    {
+      "name":"cat03",
+      "likes": 3,
+      "caption": "cat03",
+      "imagePosted": "./post's-profiles/pexels-peng-louis-1643457.jpg",
+      "comments": [],
+      "timePosted": "2h"
+    },
+    {
+      "name":"cat04",
+      "likes": 796,
+      "caption": "cat04",
+      "imagePosted": "./post's-profiles/pexels-dids-2127433.jpg",
+      "comments": [],
+      "timePosted": "3d"
+    },
+    {
+      "name":"",
+      "likes": 40,
+      "caption": "cat05",
+      "imagePosted": "./post's-profiles/pexels-alex-bargain-1472999.jpg",
+      "comments": [],
+      "timePosted": "1w"
+    }
+  ]
+
   return (
-    <p>Posts</p>
+    <div className="posts-container">
+      {postsInfo.map((post) => (
+      <div className="ind-post-container">
+        <div className="post-header">
+          <img className="post-profile-photo" />
+          <p className="post-profile-name">{post.name} - {post.timePosted}</p>
+        </div>
+        <img className="posted-image" src={require(`${post.imagePosted}`)}/>
+        <div className="bottom-post-info">
+
+        </div>
+      </div>
+      ))}
+
+    </div>
   )
 }
 
--- src/components/page/stories/Stories.jsx
+++ src/components/page/stories/Stories.jsx
@@ -35,17 +35,46 @@ const Stories = () => {
     {
       "name": "cat8",
       "photo": "./profile-photos/pexels-evg-kowalievska-1416803.jpg",
+    },
+    {
+      "name": "cat1",
+      "photo": "./profile-photos/pexels-наталья-семенкова-991831.jpg",
+    },
+    {
+      "name": "cat2",
+      "photo": "./profile-photos/pexels-tatiana-аzatskaya-3616232.jpg",
+    },
+    {
+      "name": "cat3",
+      "photo": "./profile-photos/pexels-leonardo-de-oliveira-1770918.jpg",
     }
   ]
 
+  const handleRightScroll = () =>{
+    const right = document.querySelector(".stories-container");
+    right.scrollBy(200,0);
+  }
+
+  const handleLeftScroll = () =>{
+    const left = document.querySelector(".stories-container");
+    left.scrollBy(-200,0);
+  }
+
+
   return (
-    <div className="stories-container">
-      {storiesProfiles.map((profile) => (
-        <div className="story-container">
-          <img className="profile-photos" src={require(`${profile.photo}`)} />
-          <p className="profile-names">{profile.name}</p>
-        </div>
-      ))}
+    <div className="stories-parent-container">
+      <button className="left-btn scroll-btn" onClick={handleLeftScroll}>{"<"}</button>
+      <div className="stories-container">
+        {storiesProfiles.map((profile) => (
+          <div className="story-container-gradient">
+            <div className="story-container">
+              <img className="profile-photos" src={require(`${profile.photo}`)} />
+              <p className="profile-names">{profile.name}</p>
+            </div>
+          </div>
+        ))}
+      </div>
+      <button className="right-btn scroll-btn" onClick={handleRightScroll}>{">"}</button>
     </div>
   )
 }
